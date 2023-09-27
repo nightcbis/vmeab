2c07ad6de5216d1bd5dd0b1021f6885fabb9eb25
nightcbis <andy@cbis.cc>
2023-09-27 21:08:52 +0000

    Glömde init-filen

--- custom_components/vmeab/__init__.py
+++ custom_components/vmeab/__init__.py
@@ -1,2 +1,28 @@
 """ VMEAB Sophämtning """
-from .const import DOMAIN
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, CONF_CITY, CONF_STREET
+from .scraper import vmeab_scrape
+from .coordinator import MyCoordinator
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Called from Config Flow"""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    StreetName = config_entry.data.get(CONF_STREET)
+    City = config_entry.data.get(CONF_CITY)
+
+    coordinator = MyCoordinator(hass, config_entry.data, StreetName, City)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+
+    return True
